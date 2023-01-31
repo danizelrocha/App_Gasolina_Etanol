448b4ee70d014497d7efe558ec3cbe3372a72a0f
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-01-31 16:53:27 -0300

    Primeira parte completa
    JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
+const msgResultado = document.querySelector("h2");
+
 function calcular() {
     const precoGasolina = parseFloat(document.getElementById("gasolina").value);
     const precoEtanol = parseFloat(document.getElementById("etanol").value);
 
+    if (isNaN(precoGasolina)) {
+        msgResultado.innerHTML = "Preço da Gasolina inválido"
+        return;
+    }
 
+    if (isNaN(precoEtanol)) {
+        msgResultado.innerHTML = "Preço do Etanol inválido"
+        return;
+    }
+
+    if((precoEtanol / precoGasolina) >= 0.72)
+        msgResultado.innerHTML = "Abasteça com Gasolina";
+    else    
+       msgResultado.innerHTML = "Abasteça com Etanol";
+ 
 }
