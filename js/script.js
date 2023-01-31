98c77d102bed680edfd8bc2bd5b92ea5163bc3a9
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-01-31 13:33:43 -0300

    JS

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function calcular() {
+    const precoGasolina = parseFloat(document.getElementById("gasolina").value);
+    const precoEtanol = parseFloat(document.getElementById("etanol").value);
+
+
+}
