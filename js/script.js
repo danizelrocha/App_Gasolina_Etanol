2e293c2c180ef55018ce9b5cee6445f4d8a87857
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-02-18 22:47:10 -0300

    pontuação

--- js/script.js
+++ js/script.js
@@ -11,22 +11,21 @@ veja a linha 27  */
 const msgResultado = document.querySelector("h2");
 
 function calcular() {
-    const precoGasolina = parseFloat(document.getElementById("gasolina").value);
-    const precoEtanol = parseFloat(document.getElementById("etanol").value);
+  const precoGasolina = parseFloat(document.getElementById("gasolina").value);
+  const precoEtanol = parseFloat(document.getElementById("etanol").value);
 
-    if (isNaN(precoGasolina)) {
-        msgResultado.innerHTML = "Preço da Gasolina inválido"
-        return;
-    }
+  if (isNaN(precoGasolina)) {
+    msgResultado.innerHTML = "Preço da Gasolina inválido";
+    return;
+  }
 
-    if (isNaN(precoEtanol)) {
-        msgResultado.innerHTML = "Preço do Etanol inválido"
-        return;
-    }
+  if (isNaN(precoEtanol)) {
+    msgResultado.innerHTML = "Preço do Etanol inválido";
+    return;
+  }
 
-    if((precoEtanol / precoGasolina) >= 0.72)
-        msgResultado.innerHTML = "Abasteça com Gasolina";
-    else    
-       msgResultado.innerHTML = "Abasteça com Etanol";
- 
+  if (precoEtanol / precoGasolina >= 0.72)
+    msgResultado.innerHTML = "Abasteça com Gasolina";
+  else msgResultado.innerHTML = "Abasteça com Etanol";
 }
+//acertar a pontuação no html.
